def62a9b27c3a7677dea07add6ba3db9f20c02c9
Vimal Raiyani <w3.vimal@gmail.com>
2024-09-08 10:43:15 +0530

    Header and banner completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 var $ = jQuery.noConflict();
+$mainHeaderHeight = $('.main-header').outerHeight();
 
 /* Script on ready
 ------------------------------------------------------------------------------*/
@@ -6,13 +7,46 @@ $( document ).ready( function() {
 	//do jQuery stuff when DOM is ready
 
 	/* Responsive Jquery Navigation */
-	$( '.hamburger' ).click( function( event ) {
+	$( '.hamburger' ).click(function() {
 		$( '.mobilenav' ).toggleClass( 'is-open' );
 	} );
-
 	$( '.mobilenav .nav-backdrop' ).click( function() {
 		$( '.mobilenav' ).removeClass( 'is-open' );
 	} );
+	$('.header-top-space').css('padding-top', $mainHeaderHeight);
+
+	/* header sticky */
+	$(window).scroll(function() {
+		if ($(this). scrollTop() > 1){
+			$('.main-header').addClass("sticky");
+		}
+		else{
+			$('.main-header').removeClass("sticky");
+		}
+	});
+
+	/* banner slider */
+	$('.banner-slider').owlCarousel({
+		nav:false,
+		items:1,
+		// responsive:{
+		// 	0:{
+		// 		items:1
+		// 	},
+		// 	600:{
+		// 		items:3
+		// 	},
+		// 	1000:{
+		// 		items:5
+		// 	}
+		// }
+	})
+
+/* Jump to next section */
+$( '.banner-down-btn' ).on( 'click', function(e) {
+	e.preventDefault();
+	$( 'html, body' ).animate({ scrollTop: $($( this ).attr( 'href' )).offset().top - $mainHeaderHeight + 110}, 500, 'linear' );
+});
 
 } );
 
